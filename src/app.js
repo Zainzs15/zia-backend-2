40d597d0fb941c7a603f4bbaac16eb8a7c858cdb
Zainzs15 <zsezain@gmail.com>
2026-02-03 21:14:28 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://ziahomeopethic.online",
   "https://ziahomeopethic.online/confirm-payment",
-  "https://zia-admin.vercel.app",
 ];
 
 app.use(
